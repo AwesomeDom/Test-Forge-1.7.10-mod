5ef337fe14fc26d477aaa69ebd4cdb26b1f26e37
AwesomeDom <CAinc.dom@gmail.com>
2015-01-18 01:25:09 -0500

    Added Proxies

---
+++ src/main/java/AwesomeDom/mods/BeepBoop/CommonProxy.java
@@ -0,0 +1,8 @@
+package AwesomeDom.mods.BeepBoop;
+public class CommonProxy {
+
+        // Client stuff
+        public void registerRenderers() {
+                // Nothing here as the server doesn't render graphics or entities!
+        }
+}
